03730c13ccf254c790ac34f3b2b5e89dc072b8ec
teapot4195 <huangalex409@gmail.com>
2022-06-21 21:31:51 -0400

    add wheat icon

--- prototypes/farms/wheat.lua
+++ prototypes/farms/wheat.lua
@@ -1,9 +1,14 @@
+--local functions
+local function sprite(name)
+    return '__KoS-Food-Factory__/ressources/farms/'..name
+end
+
 --item
 data:extend({
     {
         name = 'ff_farm_wheat_item',
         type = 'item',
-        icon = '__KoS-Food-Factory__/ressources/placeholder.png',
+        icon = sprite('wheat.png'),
         icon_size = 64,
         stack_size = 200,
     },
